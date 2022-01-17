581fbc49a86ade607fc03b77e2fd1a3c0a0aae3a
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-17 20:22:30 +0900

    [Feat] 기본 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CampDetail from "./pages/CampDetail";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello! This is CodeSchool Page.</h1>
+      <Routes>
+        <Route path="/codeschool/" element={<Home />} />
+        <Route path="/codeschool/detail/" element={<CampDetail />} />
+      </Routes>
     </div>
   );
 }
