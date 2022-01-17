d7225a61c6db97acb2acdefc3730e5ed51b1773e
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-17 20:32:28 +0900

    [Feat] Header Component 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <strong>CodeScool</strong>
+      <ul>
+        <li>
+          <button>profile</button>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
