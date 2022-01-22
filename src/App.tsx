ba368df51fb83a3ca69d5f5545564ef28628f2bf
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-22 15:06:54 +0900

    [remove] 불필요한 h1태그 제거

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,11 @@ import routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello! This is CodeSchool Page.</h1>
-      <Routes>
-        <Route path={routes.home} element={<Home />} />
-        <Route path={routes.campdetail} element={<CampDetail />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path={routes.home} element={<Home />} />
+      <Route path={routes.campdetail} element={<CampDetail />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
