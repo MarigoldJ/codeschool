08ed911d64c60216e071638c034e69a8066160cf
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-22 15:23:21 +0900

    [refactor] detail 하위 다중 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path={routes.home} element={<Home />} />
-      <Route path={routes.campdetail} element={<CampDetail />} />
+      <Route path={routes.campdetail + "*"} element={<CampDetail />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
