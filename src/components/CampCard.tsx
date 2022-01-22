c1e9478117513f28c6553d1f2bebc8632309c5c1
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-22 14:48:13 +0900

    [feat] CampCard 컴포넌트 추가

---
+++ src/components/CampCard.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import routes from "../routes";
+import { TypeCamp } from "../types/type";
+
+function CampCard({ camp }: { camp: TypeCamp }) {
+  return (
+    <Link to={routes.campdetail + camp.id}>
+      <div
+        style={{
+          backgroundImage: `url(${camp.classThumbnail})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div>{camp.recruitStatus}</div>
+        <div>{camp.classTitle}</div>
+        <div>{camp.classPeriod}</div>
+        <div>{camp.classStart}</div>
+      </div>
+    </Link>
+  );
+}
+
+export default CampCard;
