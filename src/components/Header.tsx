cc126ea6906617e0be6811d6813738528d109846
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-22 15:03:40 +0900

    [feat] Header에 로고, 유저아이콘 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
+import logoImage from "../asset/images/codeschool_logo.png";
+import userIcon from "../asset/images/codeschool_user_icon.png";
+
 function Header() {
   return (
     <header className="header">
-      <strong>CodeScool</strong>
-      <ul>
-        <li>
-          <button>profile</button>
-        </li>
-      </ul>
+      <img src={logoImage} alt="로고" />
+      <img src={userIcon} alt="사용자" />
     </header>
   );
 }
