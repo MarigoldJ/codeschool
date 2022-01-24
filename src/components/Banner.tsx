b1f266bc9fdc2ef7dcaf828d325a7c3dd8d301e5
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-01-24 16:11:19 +0900

    [feat] Banner 컴포넌트 스타일 생성

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,51 @@
 import { bannerImg } from "@asset/image";
+import colors from "@styles/colors";
+import { maxWidth } from "@styles/mixin";
+import styled from "styled-components";
 
 function Banner() {
   return (
-    <div>
-      <div>개발은 코드스쿨과 함께</div>
-      <img src={bannerImg} alt="logo" />
-    </div>
+    <Container>
+      <div className="banner-main">
+        <div className="banner-text">{"개발은\n코드스쿨과 함께"}</div>
+        <img className="banner-image" src={bannerImg} alt="배너" />
+      </div>
+    </Container>
   );
 }
 
 export default Banner;
+
+// Style
+const Container = styled.header`
+  padding: 104px 16px 56px;
+  box-sizing: border-box;
+  background-color: ${colors.primary1}; // TODO: Banner 배경 이미지 필요
+
+  .banner-main {
+    ${maxWidth}
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  .banner-text {
+    flex: 1;
+
+    white-space: pre-line;
+    color: ${colors.white};
+
+    font-weight: bold;
+    font-size: 40px;
+    line-height: 56px;
+  }
+
+  .banner-image {
+    flex: 2;
+    border-radius: 10px;
+    background-position: center;
+    background-size: cover;
+
+    height: 400px;
+  }
+`;
