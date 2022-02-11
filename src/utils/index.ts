a6572dca4efe3693a537a76e9562957489da36a9
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-12 01:29:47 +0900

    [feat] SERVER_URI 변수를 env에서 불러오도록 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as routes } from "./routes";
 export type { TypeCamp, FAQ } from "./type";
+export { default as SERVER_URI } from "./server";
---
+++ src/utils/server.ts
@@ -0,0 +1,6 @@
+const SERVER_URI =
+  process.env.NODE_ENV === "development"
+    ? process.env.REACT_APP_SERVER_URI_DEV
+    : process.env.REACT_APP_SERVER_URI_PROD;
+
+export default SERVER_URI;
