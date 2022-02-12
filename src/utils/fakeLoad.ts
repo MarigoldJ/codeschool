46fe7db4ed670c2f06ac848a82bd088e50f8b0f6
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-13 03:01:58 +0900

    [feat] camp가 로딩되지 않았을때 대체할 fake값 만들기

---
+++ src/utils/fakeLoad.ts
@@ -0,0 +1,16 @@
+import { TypeCamp } from "@utils/type";
+export const fakeCamp: TypeCamp = {
+  _id: "",
+  isHot: false,
+  isSpecialDiscount: false,
+  recruitStatus: "모집전",
+  category: [],
+  classTitle: "",
+  classStart: "",
+  classPeriod: "",
+  classWhere: "",
+  classStudentCapacity: 0,
+  classThumbnail: "",
+  classDetail: [],
+  classFAQ: [],
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as routes } from "./routes";
 export type { TypeCamp, FAQ } from "./type";
 export { default as SERVER_URI } from "./server";
+export { fakeCamp } from "./fakeLoad";
