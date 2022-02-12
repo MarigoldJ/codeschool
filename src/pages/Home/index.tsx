93a0ed7f08ded26bd0b102ee4a8a7319cd0cddf3
Yusang Jeon <68223294+MarigoldJ@users.noreply.github.com>
2022-02-13 00:15:45 +0900

    [remove] 필요없는 import 및 변수 제거

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,12 @@
 import { CampSection, Banner } from "./components";
 import Footer from "@components/Footer";
 import Navigation from "@components/Navigation";
-import { TypeCamp } from "@utils/type";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import SERVER_URI from "@utils/server";
 
-// TODO: 추후 실제 데이터로 변경
-const sampleCamp: TypeCamp = {
-  id: 0,
-  isHot: true,
-  isSpecialDiscount: false,
-  recruitStatus: "모집중",
-  category: ["react"],
-  classTitle: "React 실무PT",
-  classStart: "2021-01-09",
-  classPeriod: "6 weeks",
-  classWhere: "온라인",
-  classStudentCapacity: 8,
-  classThumbnail: "https://cdn.comento.kr/images/pt/tmp/prefix_16ex3CJflX.png",
-  classDetail: [],
-  classFAQ: [],
-};
-
 function Home() {
-  // TODO: useEffect를 통해 SERVER에서 불러온 데이터를 갱신
-  // const popularCamps = [sampleCamp, sampleCamp, sampleCamp, sampleCamp];
   const [popularCamps, setPopularCamps] = useState([]);
   const [saleCamps, setSaleCamps] = useState([]);
 
